92a07d7bbb70228fcb7bd5c433028095d9d29f15
nguyetvo <30364033+nguyetvo@users.noreply.github.com>
2020-03-21 00:01:23 +0700

    Update Nguyet Vo_Homework1_Option2.py

--- Nguyet Vo_Homework1_Option2.py
+++ Nguyet Vo_Homework1_Option2.py
@@ -1,7 +1,3 @@
-Python 3.7.6 (v3.7.6:43364a7ae0, Dec 18 2019, 14:18:50) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>>
 import random
 
 questions = {
